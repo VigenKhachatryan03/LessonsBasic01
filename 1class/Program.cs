d3c2fd5a12f86896384e2a6e15499508fa2ab9e7
a <a@DESKTOP-TU47UA5>
2022-11-23 18:28:58 +0300

    Add project files.

---
+++ 1class/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _1class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 15;
+            Console.WriteLine(a);
+            string Ashot = "Ashot";
+            Console.WriteLine("resd" + Ashot);
+        }
+    }
+}
